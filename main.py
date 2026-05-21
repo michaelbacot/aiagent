1f84d084508f243bc3bd4d56f3997b47d7d5f779
michaelbacot <mbacot77@gmail.com>
2026-05-20 22:22:16 -0400

    B: added agent response capability

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import argparse
+from google.genai import types
 
 def main():
     print("Hello from aiagent!")
@@ -12,7 +14,22 @@ def main():
 
     client = genai.Client(api_key=api_key)
 
-    response = client.models.generate_content(model="gemini-2.5-flash", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    parser  = argparse.ArgumentParser(description="AI Agent")
+    parser.add_argument("user_prompt", type=str, help="User prompt sent to the AI agent")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+    user_prompt = args.user_prompt
+
+    messages: list[types.Content] = [
+        types.Content(role="user", parts=[types.Part(text=args.user_prompt)])
+    ] 
+
+    response = client.models.generate_content(model="gemini-2.5-flash", contents=messages)
+    if args.verbose == True:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print("Response:")
     print(response.text)
 
 if __name__ == "__main__":
